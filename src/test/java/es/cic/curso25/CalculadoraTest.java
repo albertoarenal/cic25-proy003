7b7be13fd06d839a21623d15493034f63b601d99
noname <noname>
2025-07-09 13:41:22 +0200

    Agregada función restar

--- src/test/java/es/cic/curso25/CalculadoraTest.java
+++ src/test/java/es/cic/curso25/CalculadoraTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
+
     @Test
     public void testSumar() {
         // Preparo
@@ -23,5 +24,25 @@ public class CalculadoraTest {
         valorActual = cut.getTotal();
 
         assertEquals(9, valorActual, 0.000001);
+    }
+
+    @Test
+    public void testRestar() {
+        // Preparo
+        Calculadora cut = new Calculadora();
+
+
+        // Ejecuto
+        cut.restar(5.6);
+
+        // Verifico
+        double valorActual = cut.getTotal();
+        assertEquals(-5.6, valorActual, 0.000001);
+
+
+        cut.restar(3.4);
+        valorActual = cut.getTotal();
+
+        assertEquals(-9, valorActual, 0.000001);
     }
 }
